60d3a868abef059a9a126d97abeca856cc4fc40b
mstouk57g <KissLmisu@outlook.com>
2024-01-01 15:30:55 +0800

    andmore!

--- WaterShark/Views/ShellPage.xaml.cs
+++ WaterShark/Views/ShellPage.xaml.cs
@@ -32,7 +32,8 @@ public sealed partial class ShellPage : Page
         App.MainWindow.ExtendsContentIntoTitleBar = true;
         App.MainWindow.SetTitleBar(AppTitleBar);
         App.MainWindow.Activated += MainWindow_Activated;
-        AppTitleBarText.Text = "AppDisplayName".GetLocalized();
+        //AppTitleBarText.Text = "AppDisplayName".GetLocalized();
+        AppTitleBarText.Text = "WaterShark.AppDisplayName.GetLocalized";
     }
 
     private void OnLoaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
